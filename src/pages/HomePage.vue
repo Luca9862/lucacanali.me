69dc1997197420a5d8c06c563f70ac5c9b049830
Luca <canali.luca998@gmail.com>
2024-08-15 12:53:07 +0200

    creata HomePage, aggiunto vue-router e aggiunto element-ui

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>HomePage</h1>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+
+
+index.js
+
+import HomePage from "../pages/HomePage.vue"
+
+export const routes = [
+        {path: '/', component: HomePage},
+        {path: '*', component: {template: '<h1>Page not found</h1>'}}
+]
