e474a9e82aa82da5fc2d71e9ad1fabb1e9f2b74a
luca.canali <canali.luca998@gmail.com>
2024-08-17 15:09:31 +0200

    fix cards

--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
         <h1 style="color: wheat;">Projects</h1>
-    <el-row>
     <el-col :span="8" v-for="(project, index) in projects" :key="project" :offset="index > 0 ? 2 : 0">
-      <el-card :body-style="{ padding: '0px' }">
+      <el-card id="card" :body-style="{ padding: '0px' }">
         <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
         <div style="padding: 14px;">
           <span id="project-name">{{project.name}}</span>
@@ -13,7 +12,6 @@
         </div>
       </el-card>
     </el-col>
-  </el-row>
 </div>
 </template>   
 
@@ -42,5 +40,16 @@ export default{
 .bottom-clearfix{
     padding-top: 8px;
 }
+#card{
+  cursor: pointer;
+  width: fit-content;
+}
+.el-col, .el-col-8, .el-col-offset-0{
+  width: 100% !important;
+  text-align: -webkit-center;
+}
+
 </style>
 
+width: 100%;
+      text-align: -webkit-center;
