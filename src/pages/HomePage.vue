7d929eaee91aab1ed77c1ca7beba5d6af2f79f47
luca.canali <canali.luca998@gmail.com>
2024-08-17 14:35:59 +0200

    fix cards

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,29 +2,9 @@
     <div id="home">
         <el-row>
             <el-col :span="24"><div class="grid-content bg-purple-dark"><app-carosello></app-carosello></div></el-col>
-            </el-row>
-            <el-row>
-            <el-col :span="12"><div class="grid-content bg-purple-dark"><app-cards></app-cards></div></el-col>
-            <el-col :span="12"><div class="grid-content bg-purple-light"></div></el-col>
-            </el-row>
-            <el-row>
-            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="8"><div class="grid-content bg-purple-light"></div></el-col>
-            <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
-            </el-row>
-            <el-row>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="6"><div class="grid-content bg-purple-light"></div></el-col>
-            </el-row>
-            <el-row>
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple"></div></el-col>
-            <el-col :span="4"><div class="grid-content bg-purple-light"></div></el-col>
+        </el-row>
+        <el-row>
+            <el-col :span="24"><div class="grid-content bg-purple-dark"><app-cards></app-cards></div></el-col>
         </el-row>
     </div>
 </template>
