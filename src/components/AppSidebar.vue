0c415fb89f3088fc74b883619952ac3d7f3404f7
luca.canali <canali.luca998@gmail.com>
2024-08-22 23:06:39 +0200

    fix css

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -7,17 +7,14 @@
       </div>
 
       <!-- Menu -->
-      <el-menu
-        class="el-menu-demo"
-        mode="horizontal"
-        background-color="black"
-        text-color="#fff"
+      <el-menu class="el-menu-demo" mode="horizontal" background-color="black" text-color="#fff"
         active-text-color="#ffd04b">
         <el-menu-item @click="goToHome" style="font-size: x-large;">Luca Canali</el-menu-item>
         <el-menu-item @click="goToHome" style="font-size: large;">Home</el-menu-item>
         <el-menu-item @click="goToInfo" style="font-size: large;">Info</el-menu-item>
         <el-menu-item @click="goToProjects" style="font-size: large;">Projects</el-menu-item>
-        <el-menu-item @click="goToGithub" style="color: grey;">GitHub<el-avatar :size="30" :src="githubPicture"></el-avatar></el-menu-item>
+        <el-menu-item @click="goToGithub" style="color: grey;">GitHub<el-avatar :size="30"
+            :src="githubPicture"></el-avatar></el-menu-item>
       </el-menu>
     </div>
   </div>
@@ -37,27 +34,27 @@ export default {
     };
   },
   methods: {
-    goToEmpty(){
+    goToEmpty() {
       if (this.$route.path !== '/') {
         this.$router.push('/');
       }
     },
-    goToHome(){
+    goToHome() {
       if (this.$route.path !== '/home') {
         this.$router.push('/home');
       }
     },
-    goToInfo(){
+    goToInfo() {
       if (this.$route.path !== '/info') {
         this.$router.push('/info');
       }
     },
-    goToProjects(){
+    goToProjects() {
       if (this.$route.path !== '/projects') {
         this.$router.push('/projects');
       }
     },
-    goToGithub(){
+    goToGithub() {
       window.open(this.githubProfile, "_blank");
     }
   }
@@ -71,27 +68,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: background-color;
-}
 
-.header-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between; /* Spazio tra avatar+nome e menu */
-}
+  .header-container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    /* Spazio tra avatar+nome e menu */
+  }
 
-.avatar-name-container {
-  display: flex;
-  align-items: center;
-}
+  .avatar-name-container {
+    display: flex;
+    align-items: center;
+  }
 
-.username {
-  margin-left: 10px;
-  color: #fff;
-  font-size: 18px;
-}
+  .username {
+    margin-left: 10px;
+    color: #fff;
+    font-size: 18px;
+  }
 
-.el-menu-demo {
-  flex-grow: 1; /* Il menu si espanderà per occupare lo spazio disponibile */
-  margin-left: 20px;
+  .el-menu-demo {
+    flex-grow: 1;
+    /* Il menu si espanderà per occupare lo spazio disponibile */
+    margin-left: 20px;
+  }
 }
 </style>
--- src/components/ProjectCards.vue
+++ src/components/ProjectCards.vue
@@ -1,55 +1,68 @@
 <template>
-    <div>
-        <h1 style="color: wheat;">Projects</h1>
-    <el-col :span="8" v-for="(project, index) in projects" :key="project" :offset="index > 0 ? 2 : 0">
-      <el-card id="card" :body-style="{ padding: '0px' }">
-        <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image">
+  <div id="cards">
+    <h1 style="color: wheat;">Projects</h1>
+    <el-col :span="8" v-for="(project, index) in projects" :key="index" :offset="index > 0 ? 2 : 0">
+      <el-card class="card" :body-style="{ padding: '0px' }">
+        <img :src="project.image"
+          class="image">
         <div style="padding: 14px;">
-          <span id="project-name">{{project.name}}</span>
+          <span id="project-name">{{ project.name }}</span>
           <div class="bottom-clearfix">
             <el-button type="primary" class="button">Github Page</el-button>
           </div>
         </div>
       </el-card>
     </el-col>
-</div>
-</template>   
+  </div>
+</template>
 
 
 <script>
-export default{
-    data(){
-        return{
-            projects: [
-                {
-                    name: 'My Pets'
-                }
-            ]
+import '../assets/css/colors.css'
+import my_pet_pic from '../assets/pictures/my-pets.png'
+
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          name: 'My Pets',
+          image: my_pet_pic
+        },
+        {
+          name: 'Chess Study',
+          image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png'
+        },
+        {
+          name: 'Ciao',
+          image: 'https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png'
+
         }
+      ]
     }
+  }
 }
 </script>
 
 <style>
-
-.grid-content {
-  height: 100%;
-  text-align: center;
-}
-
-.bottom-clearfix{
-    padding-top: 8px;
-}
-#card{
-  cursor: pointer;
-  width: fit-content;
-}
-.el-col, .el-col-8, .el-col-offset-0{
-  width: 100% !important;
+#cards {
   text-align: -webkit-center;
+
+  .bottom-clearfix {
+    padding-top: 8px;
+  }
+
+  .card {
+    cursor: pointer;
+    width: fit-content;
+    height: fit-content;
+  }
+
+  .image{
+
+  }
+  .el-col-offset-2 {
+    margin-left: 0%;
+  }
 }
-
 </style>
-
-width: 100%;
-      text-align: -webkit-center;
