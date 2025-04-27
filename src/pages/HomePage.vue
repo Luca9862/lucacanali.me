e6904f7fe5e9a2ccf7e3e391e721c682b8272852
Luca <canali.luca998@gmail.com>
2025-04-27 18:39:43 +0200

    refactor(LCC-2): eliminato componente project cards e aggiunta card di vuetify in homepage

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,34 +1,81 @@
 <template>
     <div>
         <el-row>
-            <el-col :span="24"><div class="grid-content bg-purple-dark"><app-carosello></app-carosello></div></el-col>
+            <el-col :span="24">
+                <div class="grid-content bg-purple-dark"><app-carosello></app-carosello></div>
+            </el-col>
         </el-row>
         <el-row>
-            <el-col :span="24"><div class="grid-content bg-purple-dark"><app-cards></app-cards></div></el-col>
+            <el-col :span="24">
+                <div class="grid-content bg-purple-dark">
+                    <v-card class="mx-auto" max-width="344">
+                        <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+
+                        <v-card-title>
+                            My Pet
+                        </v-card-title>
+
+                        <v-card-subtitle>
+                            Android application
+                        </v-card-subtitle>
+
+                        <v-card-actions>
+                            <v-btn class="mx-2" fab dark large color="purple">
+                                <v-icon dark>
+                                    mdi-github
+                                </v-icon>
+                            </v-btn>
+
+                            <v-spacer></v-spacer>
+
+                            <v-btn icon @click="show = !show">
+                                <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                            </v-btn>
+                        </v-card-actions>
+
+                        <v-expand-transition>
+                            <div v-show="show">
+                                <v-divider></v-divider>
+
+                                <v-card-text>
+                                    I'm a thing. But, like most politicians, he promised more than he could deliver. You
+                                    won't have time for sleeping, soldier, not with all the bed making you'll be doing.
+                                    Then we'll go with that data file! Hey, you add a one and two zeros to that or we
+                                    walk! You're going to do his laundry? I've got to find a way to escape.
+                                </v-card-text>
+                            </div>
+                        </v-expand-transition>
+                    </v-card>
+                </div>
+            </el-col>
         </el-row>
         <el-row>
-            <el-col :span="24"><div class="app-info"><app-info></app-info></div></el-col>
+            <el-col :span="24">
+                <div class="app-info"><app-info></app-info></div>
+            </el-col>
         </el-row>
     </div>
 </template>
 
 <script>
 import AppCarosello from '@/components/AppCarosello.vue';
-import ProjectCards from '@/components/ProjectCards.vue';
 import InfoHome from '@/components/InfoHome.vue';
 
-export default{
-    components:{
+export default {
+    components: {
         appCarosello: AppCarosello,
-        appCards: ProjectCards,
         appInfo: InfoHome
+    },
+    data() {
+        return {
+            show: false
+        }
     }
 }
 </script>
 
 <style>
-.app-info{
+.app-info {
     padding-top: 100px;
 }
-
 </style>
