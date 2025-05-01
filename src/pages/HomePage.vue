36d00f61e76d57f6b5a52518ea577b7ab69aca6e
Luca <canali.luca998@gmail.com>
2025-05-01 22:15:26 +0200

    refactor(LCC-8): homepage cards

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,122 +1,136 @@
-<template id="home">
+<template>
     <div>
         <v-row>
             <v-col cols="12">
                 <div class="grid-content bg-purple-dark"><app-carosello></app-carosello></div>
             </v-col>
         </v-row>
-        <v-row class="cards-home" justify="center">
-            <v-col cols="12" md="4">
+
+        <v-row style="text-align: -webkit-center;">
+            <!-- Card 1 -->
+            <v-col xs="12" sm="6" md="4" class="pa-2">
                 <v-card class="mx-auto" max-width="344">
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+                    <v-img src="../assets/pictures/my-pets.png" height="200px"></v-img>
 
-                <v-card-title>
-                    My Pet
-                </v-card-title>
+                    <v-card-title>
+                        My Pet
+                    </v-card-title>
 
-                <v-card-subtitle>
-                    Android application
-                </v-card-subtitle>
+                    <v-card-subtitle>
+                        Android application
+                    </v-card-subtitle>
 
-                <v-card-actions>
-                    <v-btn class="mx-2" fab dark large color="purple">
-                    <v-icon dark>
-                        mdi-github
-                    </v-icon>
-                    </v-btn>
+                    <v-card-actions>
+                        <v-btn class="mx-2" fab dark large color="purple">
+                            <v-icon dark @click="openLink('https://github.com/Luca9862/MyPet')">
+                                mdi-github
+                            </v-icon>
+                        </v-btn>
 
-                    <v-spacer></v-spacer>
+                        <v-spacer></v-spacer>
 
-                    <v-btn icon @click="toggle(card1)">
-                    <v-icon>{{ show[index] ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                    </v-btn>
-                </v-card-actions>
+                        <v-btn icon @click="show = !show">
+                            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
+                    </v-card-actions>
 
-                <v-expand-transition>
-                    <div v-show="show.card1">
-                    <v-divider></v-divider>
-                    <v-card-text>
-                        I'm a thing. But, like most politicians, he promised more than he could deliver...
-                    </v-card-text>
-                    </div>
-                </v-expand-transition>
+                    <v-expand-transition>
+                        <div v-show="show">
+                            <v-divider></v-divider>
+                            <v-card-text>
+                                I'm a thing. But, like most politicians, he promised more than he could deliver. You
+                                won't have time for sleeping, soldier, not with all the bed making you'll be doing.
+                                Then we'll go with that data file! Hey, you add a one and two zeros to that or we
+                                walk! You're going to do his laundry? I've got to find a way to escape.
+                            </v-card-text>
+                        </div>
+                    </v-expand-transition>
                 </v-card>
             </v-col>
 
-            <v-col cols="12" md="4">
+            <!-- Card 2 -->
+            <v-col xs="12" sm="6" md="4" class="pa-2">
                 <v-card class="mx-auto" max-width="344">
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+                    <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
 
-                <v-card-title>
-                    My Pet
-                </v-card-title>
+                    <v-card-title>
+                        pgn-manager
+                    </v-card-title>
 
-                <v-card-subtitle>
-                    Android application
-                </v-card-subtitle>
+                    <v-card-subtitle>
+                        Python package
+                    </v-card-subtitle>
 
-                <v-card-actions>
-                    <v-btn class="mx-2" fab dark large color="purple">
-                    <v-icon dark>
-                        mdi-github
-                    </v-icon>
-                    </v-btn>
+                    <v-card-actions>
+                        <v-btn class="mx-2" fab dark large color="purple">
+                            <v-icon dark @click="openLink('https://github.com/Luca9862/pgn_manager_lib')">
+                                mdi-github
+                            </v-icon>
+                        </v-btn>
 
-                    <v-spacer></v-spacer>
+                        <v-spacer></v-spacer>
 
-                    <v-btn icon @click="toggle(card2)">
-                    <v-icon>{{ show[index] ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                    </v-btn>
-                </v-card-actions>
+                        <v-btn icon @click="show = !show">
+                            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
+                    </v-card-actions>
 
-                <v-expand-transition>
-                    <div v-show="show.card2">
-                    <v-divider></v-divider>
-                    <v-card-text>
-                        I'm a thing. But, like most politicians, he promised more than he could deliver...
-                    </v-card-text>
-                    </div>
-                </v-expand-transition>
+                    <v-expand-transition>
+                        <div v-show="show">
+                            <v-divider></v-divider>
+                            <v-card-text>
+                                I'm a thing. But, like most politicians, he promised more than he could deliver. You
+                                won't have time for sleeping, soldier, not with all the bed making you'll be doing.
+                                Then we'll go with that data file! Hey, you add a one and two zeros to that or we
+                                walk! You're going to do his laundry? I've got to find a way to escape.
+                            </v-card-text>
+                        </div>
+                    </v-expand-transition>
                 </v-card>
             </v-col>
 
-            <v-col cols="12" md="4">
+            <!-- Card 3 -->
+            <v-col xs="12" sm="6" md="4" class="pa-2">
                 <v-card class="mx-auto" max-width="344">
-                <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+                    <v-img src="../assets/pictures/koivisto.png" height="200px"></v-img>
 
-                <v-card-title>
-                    My Pet
-                </v-card-title>
+                    <v-card-title>
+                        Chess Engine Study - Koivisto
+                    </v-card-title>
 
-                <v-card-subtitle>
-                    Android application
-                </v-card-subtitle>
+                    <v-card-subtitle>
+                        Degree thesis
+                    </v-card-subtitle>
 
-                <v-card-actions>
-                    <v-btn class="mx-2" fab dark large color="purple">
-                    <v-icon dark>
-                        mdi-github
-                    </v-icon>
-                    </v-btn>
+                    <v-card-actions>
+                        <v-btn class="mx-2" fab dark large color="purple">
+                            <v-icon dark @click="openLink('https://github.com/Luca9862/ChessEngineStudy_Koivisto')">
+                                mdi-github
+                            </v-icon>
+                        </v-btn>
 
-                    <v-spacer></v-spacer>
+                        <v-spacer></v-spacer>
 
-                    <v-btn icon @click="toggle(card3)">
-                    <v-icon>{{ show[index] ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-                    </v-btn>
-                </v-card-actions>
+                        <v-btn icon @click="show = !show">
+                            <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+                        </v-btn>
+                    </v-card-actions>
 
-                <v-expand-transition>
-                    <div v-show="show.card3">
-                    <v-divider></v-divider>
-                    <v-card-text>
-                        I'm a thing. But, like most politicians, he promised more than he could deliver...
-                    </v-card-text>
-                    </div>
-                </v-expand-transition>
+                    <v-expand-transition>
+                        <div v-show="show">
+                            <v-divider></v-divider>
+                            <v-card-text>
+                                I'm a thing. But, like most politicians, he promised more than he could deliver. You
+                                won't have time for sleeping, soldier, not with all the bed making you'll be doing.
+                                Then we'll go with that data file! Hey, you add a one and two zeros to that or we
+                                walk! You're going to do his laundry? I've got to find a way to escape.
+                            </v-card-text>
+                        </div>
+                    </v-expand-transition>
                 </v-card>
             </v-col>
         </v-row>
+
         <v-row>
             <v-col cols="12">
                 <div class="app-info"><app-info></app-info></div>
@@ -128,6 +142,7 @@
 <script>
 import AppCarosello from '@/components/AppCarosello.vue';
 import InfoHome from '@/components/InfoHome.vue';
+import koivisto from '../assets/pictures/koivisto.png'
 
 export default {
     components: {
@@ -136,25 +151,19 @@ export default {
     },
     data() {
         return {
-            card1: false,
-            card2: false,
-            card3: false
+            show: false,
+            koivisto: koivisto
         }
     },
-    methods: {
-        toggle(cardKey) {
-            this.show[cardKey] = !this.show[cardKey];
-        }
+    methods:{
+        openLink(url) {
+            window.open(url, '_blank');
+    }
     }
 }
 </script>
 
-<style scoped>
-.row + .row {
-        margin-top: 12px;
-        justify-self: anchor-center;
-    }
-
+<style>
 .app-info {
     padding-top: 100px;
 }
