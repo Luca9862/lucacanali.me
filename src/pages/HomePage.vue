17321769eb16e72b5f261a41472a0b4c18b5f37d
luca.canali <canali.luca998@gmail.com>
2025-05-09 22:39:09 +0200

    refactor(LCC-8): cambiate icone progetti

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -10,7 +10,7 @@
             <!-- Card 1 -->
             <v-col xs="12" sm="6" md="4" class="pa-2">
                 <v-card class="mx-auto" max-width="344">
-                    <v-img src="../assets/pictures/my-pets.png" height="200px"></v-img>
+                    <v-img src="../assets/pictures/myPet.png" height="200px"></v-img>
 
                     <v-card-title>
                         My Pet
@@ -51,7 +51,7 @@
             <!-- Card 2 -->
             <v-col xs="12" sm="6" md="4" class="pa-2">
                 <v-card class="mx-auto" max-width="344">
-                    <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+                    <v-img src="../assets/pictures/pgnManager.png" height="200px"></v-img>
 
                     <v-card-title>
                         pgn-manager
@@ -79,10 +79,7 @@
                         <div v-show="showSecondCard">
                             <v-divider></v-divider>
                             <v-card-text>
-                                I'm a thing. But, like most politicians, he promised more than he could deliver. You
-                                won't have time for sleeping, soldier, not with all the bed making you'll be doing.
-                                Then we'll go with that data file! Hey, you add a one and two zeros to that or we
-                                walk! You're going to do his laundry? I've got to find a way to escape.
+                                A user-friendly Python library for basic PGN (chess) file handling - PGN stands for Portable Game Notation, a standard plain text format for recording chess games. It includes the moves of the game as well as metadata such as the players, the event, the date, and the result.
                             </v-card-text>
                         </div>
                     </v-expand-transition>
