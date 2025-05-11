3e2b89933013a68ae76ea5e06b24729b841a7fe9
luca.canali <canali.luca998@gmail.com>
2025-05-11 18:55:19 +0200

    feat(LCC-3): creata pagina projects

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -11,6 +11,7 @@
         active-text-color="#ffd04b">
         <el-menu-item @click="goToHome" style="font-size: x-large;">Luca Canali</el-menu-item>
         <el-menu-item @click="goToHome" style="font-size: large;">Home</el-menu-item>
+        <el-menu-item @click="goToProjects" style="font-size: large;">Projects</el-menu-item>
         <el-menu-item @click="goToInfo" style="font-size: large;">Info</el-menu-item>
         <el-menu-item @click="goToGithub" style="color: grey;"><span style="padding-right: 8%;">GitHub</span><el-avatar :size="25" :src="githubPicture" style="background-color: black;"></el-avatar></el-menu-item>
       </el-menu>
@@ -40,6 +41,11 @@ export default {
     goToHome() {
       if (this.$route.path !== '/home') {
         this.$router.push('/home');
+      }
+    },
+    goToProjects() {
+      if (this.$route.path !== '/projects') {
+        this.$router.push('/projects');
       }
     },
     goToInfo() {
