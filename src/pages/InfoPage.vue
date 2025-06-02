a85e674e308e2140f2d0832c98073eb2b0ee469d
Luca <canali.luca998@gmail.com>
2025-06-02 15:23:15 +0200

    Revert "feat(LCC-9): aggiunte nuove icons in info page"
    This reverts commit 29a67b4dd5121b3f8351dd937e23d70e84e749af.

--- src/pages/InfoPage.vue
+++ src/pages/InfoPage.vue
@@ -33,30 +33,17 @@
                 </v-card>
             </v-timeline-item>
         </v-timeline>
-        <p style="text-align: center; padding-top: 100px ;color: white;">My name is Luca, I’m 26 years old and a
-            full-stack developer with
-            solid experience in back-end, front-end, and database management. Over the years, I’ve been designing and
-            developing web applications, combining technical expertise with attention to detail to deliver tailor-made
-            and functional solutions.<br><br>
-            I have worked in both structured teams and independently, collaborating with companies and private clients
-            on projects of various kinds. I also occasionally work as a freelancer, offering personalized consulting and
-            developing digital products that meet specific client needs.<br><br>
-            I’m passionate about technology and continuously learning — I enjoy exploring new frameworks, programming
-            languages, and tools that can enhance the quality of my work and make me more versatile and effective. I
-            believe in lifelong learning and knowledge sharing, values that I bring into every project I take
-            on.<br><br>
-            My goal is to create modern, high-performance, and user-friendly applications that make a real impact in the
-            digital world.
+        <p style="padding-top: 100px ;color: white;">I am 26 years old and a full-stack developer with experience in
+            back-end, front-end,
+            and databases. I occasionally work as a freelancer, building web applications and providing tailored
+            solutions for various technological needs. I am passionate about technology and continually improving my
+            skills by exploring new technologies and frameworks.
         </p>
         <div class="grid-content bg-purple-dark" style="padding-top: 100px; text-align: center;">
             <el-avatar :size="80" :src="vueIcon" style="background-color: black;"></el-avatar>
             <el-avatar :size="80" :src="javaIcon" style="background-color: black;"></el-avatar>
-            <el-avatar :size="80" :src="pythonIcon" style="background-color: black;"></el-avatar>
             <el-avatar :size="80" :src="cSharpIcon" style="background-color: black;"></el-avatar>
             <el-avatar :size="80" :src="flutterIcon" style="background-color: black;"></el-avatar>
-            <el-avatar :size="80" :src="kotlinIcon" style="background-color: black;"></el-avatar>
-
-
         </div>
     </div>
 
@@ -68,8 +55,8 @@ import javaPic from '../assets/icons/icons8-java.svg'
 import cSharpPic from '../assets/icons/icons8-c-sharp-logo.svg'
 import flutterPic from '../assets/icons/icons8-flutter.svg'
 import linkedingPic from '../assets/icons/icons8-linkedin.svg'
-import pythonPic from '../assets/icons/icons8-python.svg'
-import kotlinPic from '../assets/icons/icons8-kotlin.svg'
+
+
 
 export default {
     data() {
@@ -96,9 +83,7 @@ export default {
             javaIcon: javaPic,
             cSharpIcon: cSharpPic,
             flutterIcon: flutterPic,
-            linkedinIcon: linkedingPic,
-            pythonIcon: pythonPic,
-            kotlinIcon: kotlinPic
+            linkedinIcon: linkedingPic
         }
     },
     components: {
