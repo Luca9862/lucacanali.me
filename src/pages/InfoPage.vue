d58f72324f94696bbfd36406b3eba23585c116e8
luca.canali <canali.luca998@gmail.com>
2025-06-02 15:55:03 +0200

    refactor(LCC-9): info page

--- src/pages/InfoPage.vue
+++ src/pages/InfoPage.vue
@@ -113,4 +113,9 @@ export default {
     justify-self: anchor-center;
     /* aumenta lo spessore */
 }
+@media (max-width: 600px) {
+  .v-timeline-item__dot {
+    display: none !important;
+  }
+}
 </style>
